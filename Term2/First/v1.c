ff6d49e8549db23e18a05cf19f74b29ed50df124
ProMix <misha.zavertkin@mail.ru>
2023-02-14 16:53:44 +0700

    Complete first task

--- Term2/First/v1.c
+++ Term2/First/v1.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int sgn(float x) {
+int sgn(float x) { // определение функции
     asm(
-        "shl eax,1;"
-        "jz sgn_end;"
-        "sbb eax,eax;"
-        "or al,1;"
+        "shl eax,1;" // двигаемся влево на 1 бит
+        "jz sgn_end;" // если рез. последней операции равен 0, то переходим к метке "sgn_end"
+        "sbb eax,eax;" // вычитаем число из самого себя, затем из результата вычитаем CARRY FLAG
+        "or al,1;" // побитовое ИЛИ
 
-        "sgn_end:");
+        "sgn_end:"); // МЕТКА завершения программы 
 }
 
 void main() {
