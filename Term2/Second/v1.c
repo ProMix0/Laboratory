30ca99510cfcdf87d9ca13368a8ac5de7e22a74d
ProMix0 <misha.zavertkin@mail.ru>
2023-03-22 11:52:16 +0700

    Second lab completed

--- Term2/Second/v1.c
+++ Term2/Second/v1.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
-void abs_arr(long s[], unsigned long d[], int count){
+void abs_arr(unsigned long d[], long s[], int count){
 asm(
-    "mov ecx, DWORD PTR 20[bp];\n"
+    "mov ecx, edx;\n"
     "jecxz abs_exit;\n"
-    "mov esi, QWORD PTR 16[bp];\n"
-    "mov edi, QWORD PTR 8[bp];\n"
     "abs_loop:\n"
-    "lodsd;\n"
+    "lodsq;\n"
     "cdq;\n"
     "xor eax, edx;\n"
     "sub eax, edx;\n"
-    "stosd;\n"
+    "stosq;\n"
     "loop abs_loop;\n"
     "abs_exit:\n"
 );
@@ -20,9 +18,10 @@ asm(
 void main(){
     long from[16]={
         1,-2,3,-4,5,-6,7,-8,9,-10,11,-12,13,-14,15,-16
-    },to[16];
-    abs_arr(from, to, 16);
+    };
+    unsigned long to[16];
+    abs_arr(to, from, 16);
     for(int i=0;i<16;i++){
-        printf("%ld\t%ld\n",from[i],to[i]);
+        printf("%ld\t%lu\n",from[i],to[i]);
     }
 }
