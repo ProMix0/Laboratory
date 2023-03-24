191ddfc2a64020bb0e6eebffae00cfb4fb759d90
ProMix0 <misha.zavertkin@mail.ru>
2023-03-24 11:38:55 +0700

    Commentaries added

--- Term2/Third/v1.c
+++ Term2/Third/v1.c
@@ -3,13 +3,13 @@
 unsigned long f(unsigned char n)
 {
 asm(
-"mov ecx,eax; \n"
+"mov ecx,eax; \n" // Дублирование числа в ecx
 "f_loop:; \n"
-"dec ecx; \n"
-"jle f_end; \n"
-"mul ecx; \n"
-"jnc f_loop; \n"
-"xor eax,eax; \n"
+"dec ecx; \n" // -1
+"jle f_end; \n" // Выход из цикла если ecx <= 0
+"mul ecx; \n" // Умножает eax на значение ecx
+"jnc f_loop; \n" // Повторяем цикл если факториал помещается в регистре
+"xor eax,eax; \n" // Обнуление eax
 "f_end:; \n"
 );
 }
