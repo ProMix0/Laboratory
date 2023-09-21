778046a43f721b4feb15df01ba979f26112d494c
ProMix0 <misha.zavertkin@mail.ru>
2023-09-21 09:35:14 +0700

    Intends and destination file memory clear

--- Term3/First/v10.c
+++ Term3/First/v10.c
@@ -1,48 +1,48 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        printf("Wrong arguments count\n");
-        return -1;
-    }
+	if (argc < 3) {
+		printf("Wrong arguments count\n");
+		return -1;
+	}
 
-    char *fileName = argv[1];
-    int limit = atoi(argv[2]);
+	char *fileName = argv[1];
+	int limit = atoi(argv[2]);
 
-    int inputFile = open(fileName, O_RDONLY);
-    if (inputFile < 0) {
-        printf("Fail to open file\n");
-        return -1;
-    }
+	int inputFile = open(fileName, O_RDONLY);
+	if (inputFile < 0) {
+		printf("Fail to open file\n");
+		return -1;
+	}
 
-    char *destinationFileName = malloc(strlen(fileName) + 4);
-    strcpy(destinationFileName, fileName);
-    int outputFile = creat(strcat(destinationFileName, ".out"), O_WRONLY);
-    if (outputFile < 0) {
-        printf("Fail to write to %s\n", destinationFileName);
-        return -1;
-    }
+	char destinationFileName[strlen(fileName) + 5];
+	strcpy(destinationFileName, fileName);
+	int outputFile = creat(strcat(destinationFileName, ".out"), O_WRONLY);
+	if (outputFile < 0) {
+		printf("Fail to write to %s\n", destinationFileName);
+		return -1;
+	}
 
-    int replace_count = 0;
-    int first = 0;
-    read(inputFile, &first, 1);
-    int c;
-    while (read(inputFile, &c, 1)) {
-        if (c == ' ' && limit > 0) {
-            write(outputFile, &first, 1);
-            replace_count++;
-            limit--;
-        } else {
-            write(outputFile, &c, 1);
-        }
-    }
+	int replace_count = 0;
+	int first = 0;
+	read(inputFile, &first, 1);
+	int c;
+	while (read(inputFile, &c, 1)) {
+		if (c == ' ' && limit > 0) {
+			write(outputFile, &first, 1);
+			replace_count++;
+			limit--;
+		} else {
+			write(outputFile, &c, 1);
+		}
+	}
 
-    close(inputFile);
-    close(outputFile);
+	close(inputFile);
+	close(outputFile);
 
-    return replace_count;
+	return replace_count;
 }
--- Term3/First/v14.c
+++ Term3/First/v14.c
@@ -1,45 +1,45 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        printf("Wrong arguments count\n");
-        return -1;
-    }
+	if (argc < 3) {
+		printf("Wrong arguments count\n");
+		return -1;
+	}
 
-    char *fileName = argv[1];
-    char toReplace = *argv[2];
+	char *fileName = argv[1];
+	char toReplace = *argv[2];
 
-    int inputFile = open(fileName, O_RDONLY);
-    if (inputFile < 0) {
-        printf("Fail to open file\n");
-        return -1;
-    }
+	int inputFile = open(fileName, O_RDONLY);
+	if (inputFile < 0) {
+		printf("Fail to open file\n");
+		return -1;
+	}
 
-    char *destinationFileName = malloc(strlen(fileName) + 4);
-    strcpy(destinationFileName, fileName);
-    int outputFile = creat(strcat(destinationFileName, ".out"), O_WRONLY);
-    if (outputFile < 0) {
-        printf("Fail to write to %s\n", destinationFileName);
-        return -1;
-    }
+	char destinationFileName[strlen(fileName) + 5];
+	strcpy(destinationFileName, fileName);
+	int outputFile = creat(strcat(destinationFileName, ".out"), O_WRONLY);
+	if (outputFile < 0) {
+		printf("Fail to write to %s\n", destinationFileName);
+		return -1;
+	}
 
-    int replace_count = 0, replaces = 0;
-    int c;
-    while (read(inputFile, &c, 1)) {
-        if (c >= '0' && c <= '9') {
-            write(outputFile, &toReplace, 1);
-            replaces++;
-        } else {
-            write(outputFile, &c, 1);
-        }
-    }
+	int replaces = 0;
+	int c;
+	while (read(inputFile, &c, 1)) {
+		if (c >= '0' && c <= '9') {
+			write(outputFile, &toReplace, 1);
+			replaces++;
+		} else {
+			write(outputFile, &c, 1);
+		}
+	}
 
-    close(inputFile);
-    close(outputFile);
+	close(inputFile);
+	close(outputFile);
 
-    return replaces;
+	return replaces;
 }
