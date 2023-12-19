14dd833e5e166ec272ac16e5ebf26a4e6f7900ad
ProMix0 <misha.zavertkin@mail.ru>
2023-12-19 17:00:34 +0700

    RGZ v14 done +fixes

--- Term3/rgz/v14/daemon.c
+++ Term3/rgz/v14/daemon.c
@@ -1,3 +1,4 @@
+#include <arpa/inet.h>
 #include <fcntl.h>
 #include <signal.h>
 #include <stdio.h>
@@ -7,23 +8,27 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#include "common.h"
-
 int process_args(char *arg);
 void do_cleanup(int sig);
 
-int main() {
-	mkfifo(ARG_PIPE, 0666);
-	mkfifo(RES_PIPE, 0666);
+int sock;
 
-	int arg_fd = open(ARG_PIPE, O_RDONLY);
-	if (arg_fd < 0) {
-		fprintf(stderr, "Unable to open argument pipe\n");
+int main() {
+	struct sockaddr_in address, client_address;
+	unsigned int addr_len = sizeof(address);
+	memset(&address, 0, sizeof(address));
+	memset(&client_address, 0, sizeof(address));
+	address.sin_family = AF_INET;
+	address.sin_port = htons(8448);
+	address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+		fprintf(stderr, "Fail to create socket\n");
 		return -1;
 	}
-	int res_fd = open(RES_PIPE, O_WRONLY);
-	if (res_fd < 0) {
-		fprintf(stderr, "Unable to open result pipe\n");
+
+	if (bind(sock, &address, sizeof(address))) {
+		fprintf(stderr, "Fail to bind socket\n");
 		return -1;
 	}
 
@@ -31,31 +36,31 @@ int main() {
 
 	while (1) {
 		fprintf(stderr, "Waiting for client connection\n");
-
 		char buf[1024];
-		read(arg_fd, buf, 1024);
-		fprintf(stderr, "Arguments received: \"%s\"\nStart processing\n", buf);
 
+		int len =
+			recvfrom(sock, buf, sizeof(buf), 0, &client_address, &addr_len);
+
+		fprintf(stderr, "Arguments received: \"%s\"\nStart processing\n", buf);
 		int result = process_args(buf);
 		fprintf(stderr, "Result: %d\n", result);
 
 		fprintf(stderr, "Sending result\n");
-		write(res_fd, &result, sizeof(int));
+		sendto(sock, &result, sizeof(int), 0, &client_address, addr_len);
 	}
 }
 
 void do_cleanup(int sig) {
 	fprintf(stderr, "Stopping service\n");
-	unlink(ARG_PIPE);
-	unlink(RES_PIPE);
+	close(sock);
 	exit(0);
 }
 
 int process_args(char *arg) {
 	char fileName[256];
-	char toDelete;
+	char toReplace;
 
-	if (sscanf(arg, "%s %c", fileName, &toDelete) != 2) {
+	if (sscanf(arg, "%s %c", fileName, &toReplace) != 2) {
 		fprintf(stderr, "Unable to read args from memory\n");
 		return -1;
 	}
@@ -75,18 +80,19 @@ int process_args(char *arg) {
 		return -1;
 	}
 
-	int deletions = 0;
+	int replaces = 0;
 	int c = 0;
 	while (read(inputFile, &c, 1)) {
-		if (c != toDelete) {
-			write(outputFile, &c, 1);
+		if (c >= '0' && c <= '9') {
+			write(outputFile, &toReplace, 1);
+			replaces++;
 		} else {
-			deletions++;
+			write(outputFile, &c, 1);
 		}
 	}
 
 	close(inputFile);
 	close(outputFile);
 
-	return deletions;
+	return replaces;
 }
