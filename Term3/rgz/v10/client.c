efe382577602b28bd0eba6545da90b09f383f140
ProMix0 <misha.zavertkin@mail.ru>
2023-12-19 13:29:41 +0700

    RGZ v10 done

---
+++ Term3/rgz/v10/client.c
@@ -0,0 +1,52 @@
+#include "common.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <sys/msg.h>
+#include <stdio.h>
+
+int main(int argc, char*argv[]){
+	if (argc < 3) {
+		printf("Wrong arguments count\n");
+		return -1;
+	}
+
+	char *fileName = argv[1];
+	int limit = atoi(argv[2]);
+
+	int sh_file = open(SH_FILE, O_RDWR);
+	if(sh_file < 0){
+		printf("Unable to open shared file: \"" SH_FILE"\"\n");
+		return -1;}
+	char *map_mem = mmap(0, 1024, PROT_WRITE | PROT_READ, MAP_SHARED, sh_file, 0);
+	if(map_mem == MAP_FAILED){
+		printf("Unable to map shared file\n");
+		return errno;}
+
+	int queue = msgget(MSG_QUEUE, 0666);
+	if(queue < 0){
+		printf("Unable to open message queue\n");
+		return -1;}
+	message_t message;
+	
+	message.type = WRITE_REQUEST;
+	msgsnd(queue, &message, 0, 0);
+	
+	char cwd[256];
+	getcwd(cwd, sizeof(cwd));
+	snprintf(map_mem, 1024, "%s/%s %d", cwd, fileName, limit);
+
+	message.type = WRITE_ACK;
+	msgsnd(queue, &message, 0, 0);
+	msgrcv(queue, 0, 0, READ_RESULT, 0);
+
+	int result = *((int*)map_mem);
+	message.type = READ_DONE;
+	msgsnd(queue, &message, 0, 0);
+
+	printf("Result: %d\n", result);
+	return 0;
+}
