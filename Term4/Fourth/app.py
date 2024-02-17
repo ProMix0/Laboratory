153ed9b837ec6736a54e4a2539c7ce1ad29ca668
Misha Zavertkin <misha.zavertkin@mail.ru>
2024-02-17 20:17:32 +0700

    Lab4 done

--- Term4/Fourth/app.py
+++ Term4/Fourth/app.py
@@ -17,22 +17,48 @@ def close_connection(exception):
 
 @app.route("/")
 def list_entries():
-    result = ""
-    cursor = get_db().cursor()
-    for (name, repo, id) in cursor.execute("SELECT name, repo, ROWID FROM portfolio"):
+    result = '<a href="/new"><h1>Add new</h1></a>'
+    connection = get_db()
+    for (name, repo, id) in connection.execute("SELECT name, repo, ROWID FROM portfolio"):
         result += f'<p>Name: <a href="/{id}">{name}</a>, repo: <a href="{repo}">{repo}</a></p>'
     return result
 
 @app.get("/<int:id>")
 def user_get(id):
-    result = ""
-    cursor = get_db().cursor()
-    row = cursor.execute("SELECT name, repo FROM portfolio WHERE ROWID = ?", (id, )).fetchone()
+    result = '<a href="/"><h1>Home</h1></a>'
+    connection = get_db()
+    row = connection.execute("SELECT name, repo FROM portfolio WHERE ROWID = ?", (id, )).fetchone()
     if row is None:
         return "Unexisting ID!"
     (name, repo) = row
     result += f'<p>Name: {name}, repo: <a href="{repo}">{repo}</a></p>'
+    result += f"""
+    <div class="container mt-5">
+        <form method="POST">
+            <input type="text" name="name" id="name" class="form-control" value="{name}"><br>
+            <textarea name="repo" id="repo" class="form-control">{repo}</textarea><br>
+            <input type="submit" class="btn btn-success" value="Update">
+        </form>
+        <a href="/{id}/delete" data-method="delete">Delete</a>
+    </div>
+    """
     return result
+
+@app.post("/<int:id>")
+def user_update(id):
+    name = request.form['name']
+    repo = request.form['repo']
+    connection = get_db()
+    connection.execute("UPDATE portfolio SET name = ?, repo = ? WHERE ROWID = ?", (name, repo, id))
+    connection.commit()
+    return redirect(url_for('user_get', id=id))
+
+@app.get("/<int:id>/delete")
+def user_delete(id):
+    connection = get_db()
+    connection.execute("DELETE FROM portfolio WHERE ROWID = ?", (id, ))
+    connection.commit()
+    return redirect(url_for('list_entries'))
 
 @app.get("/new")
 def new_user_get():
@@ -51,7 +77,7 @@ def new_user_get():
 def new_user_post():
     name = request.form['name']
     repo = request.form['repo']
-    cursor = get_db().cursor()
-    cursor.execute('INSERT INTO portfolio VALUES (?, ?)', (name, repo))
-    get_db().commit()
-    return redirect(url_for('user', id=cursor.lastrowid))
+    connection = get_db()
+    connection.execute('INSERT INTO portfolio VALUES (?, ?)', (name, repo))
+    connection.commit()
+    return redirect(url_for('user_get', id=cursor.lastrowid))
