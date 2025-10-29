986b6a33ee21476427c4ab2ec3c4848dc547a365
Michael Zavertkin <misha.zavertkin@mail.ru>
2025-10-29 12:48:03 +0700

    flp: lab3 fix

--- Term7/flp/lab3/lab.rkt
+++ Term7/flp/lab3/lab.rkt
@@ -1,16 +1,13 @@
 #lang racket
 
 ;;  Функция для вычисления суммы чётных чисел от 2 до n
-(define (sum-even n)
-  (define (helper i acc)
-    (if (> i n)
-        acc
-        (helper (+ i 2) (+ acc i))))
-  (if (< n 2)
-      0
-      (helper 2 0)))
+(define (my-sum-even n)
+  (cond
+    [(< n 2) 0]
+    [(= 1 (remainder n 2)) (my-sum-even (- n 1))]
+    [else (+ n (my-sum-even (- n 2)))]))
 
-(displayln (string-append "Сумма чётных чисел: " (number->string (sum-even 17))))
+(displayln (string-append "Сумма чётных чисел: " (number->string (my-sum-even 17))))
 
 ;;  Функция для вставки элемента в список на указанную позицию
 (define (insert-at lst elem pos)
@@ -22,12 +19,11 @@
 (displayln (string-append "Список после вставки: " (format "~a" (insert-at '(1 2 3 4) 99 2))))
 
 ;;  Функция для подсчёта числа элементов списка
-(define (my-length lst)
-  (define (helper lst acc)
-    (if (null? lst)
-        acc
-        (helper (cdr lst) (+ acc 1))))
-  (helper lst 0))
+(define (my-length lst elem)
+  (cond
+    [(null? lst) 0]
+    [(= elem (car lst)) (my-length (cdr lst) elem)]
+    [else (+ 1 (my-length (cdr lst) elem))]))
 
-(displayln (string-append "Количество элементов: " (number->string (my-length '(a b c d e)))))
+(displayln (string-append "Количество элементов: " (number->string (my-length '(1 2 3 4 5) 4))))
 
